dc5efb92f2cd323ceb1321d24290e52142501ed4
Ashad Nadeem Mahmudi <ashadnadeem@gmail.com>
2021-11-28 13:50:58 +0500

    login temp

--- src/App.js
+++ src/App.js
@@ -1,32 +1,15 @@
-import logo from './logo.svg';
-import { useEffect, useState } from 'react'
+import React from 'react';
 import './App.css';
-import Header from './Header'
-import Home from './Home'
-import Cart from './Cart'
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import Header from './Header';
 
 function App() {
   return (
     // bem
     <Router>
-      <div className="App">
-        <Header 
-          cartItems={cartItems} />
-        <Switch>
-          <Route path="/cart">
-            <Cart cartItems={cartItems} />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
+    <div className="App">
+      <Header />
+    {/*Home */}
+    </div>
     </Router>
   );
 }
